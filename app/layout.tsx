f42c42238e26949a57e2bcbd120281535df60117
Sergio Nezhigay <abc1971h@gmail.com>
2023-11-27 20:19:55 +0200

    font rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Oswald, Fira_Sans } from "next/font/google";
+import { Oswald, Fira_Sans as firaSans } from "next/font/google";
 import "./globals.css";
 
-const firasans = Fira_Sans({
+const firasans = firaSans({
   weight: "400",
   subsets: ["latin"],
   display: "swap",
