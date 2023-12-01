c3c4cc4e7636d3ce8e3d37f888a0bd311c235ef7
Sergio Nezhigay <abc1971h@gmail.com>
2023-12-01 18:16:06 +0200

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import About from "@/components/about/About";
 import Main from "@/components/main/Main";
 import Electricity from "@/components/Electricity";
 import Cases from "@/components/cases/Cases";
-// import Faq from "@/components/faq/Faq";
 import ContactUs from "@/components/contactUs/ContactUs";
 import Test from "@/components/test/Test";
 
@@ -13,7 +12,6 @@ export default function Home() {
       <About />
       <Electricity />
       <Cases />
-      {/* <Faq /> */}
       <Test />
       <ContactUs />
     </>
