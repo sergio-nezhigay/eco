b431be9bb2f115077287888fff13128665b6b25f
Sergio Nezhigay <abc1971h@gmail.com>
2023-12-01 19:04:54 +0200

    bad test

--- components/contactUs/ContactForm.tsx
+++ components/contactUs/ContactForm.tsx
@@ -5,9 +5,18 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 import formSchema from "./formSchema";
-import { Form, FormField } from "@/components/ui/form";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
 import SecondaryButton from "../main/SecondaryButton";
 import Field from "./Field";
+
+import { Input } from "../ui/input";
 
 export function ProfileForm() {}
 
@@ -52,7 +61,30 @@ function ContactForm() {
         onSubmit={form.handleSubmit(onSubmit)}
         className="bg-primary-500 px-3 py-9 md:w-1/2 md:px-6 lg:p-12"
       >
-        <FormField control={form.control} name="username" render={test} />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            // <Field
+            //   fieldName="* Full name:"
+            //   field={field}
+            //   placeholder="John Rosie"
+            //   error={formState.errors.username?.message}
+            // />
+
+            <FormItem className={`mb-7 space-y-0 `}>
+              <FormLabel className="paragraph-regular  ">zdsfhedyh</FormLabel>
+              <FormControl className={`rounded-none  bg-transparent`}>
+                <Input
+                  {...field}
+                  placeholder="dxgfh"
+                  className="px-0 py-2 text-[18px] leading-[21.6px] tracking-m4 outline-0  ring-current placeholder:text-primary-700 focus:ring-1 lg:text-[20px] lg:leading-[24px]"
+                />
+              </FormControl>
+              <FormMessage className="pt-3 text-right text-[12px] font-normal text-[#D28B8B]" />
+            </FormItem>
+          )}
+        />
         {/* <FormField
           control={form.control}
           name="username"
