ffa1dabc541423c251104e0624b340f1c718d70e
Sergio Nezhigay <abc1971h@gmail.com>
2023-12-02 21:08:20 +0200

    border correct

--- components/common/SecondaryButton.tsx
+++ components/common/SecondaryButton.tsx
@@ -3,7 +3,7 @@ import Icon from "../ui/Icon";
 
 function SecondaryButton({ content }: { content: string }) {
   return (
-    <span className="flex-center mx-auto flex w-fit gap-3  rounded-full border border-primary-300 bg-transparent py-[3.5px] pl-4 pr-1 text-base font-normal leading-[18px] tracking-m4 hover:bg-primary-200 hover:text-primary-300  md:ml-0">
+    <span className="flex-center mx-auto flex w-fit gap-3  rounded-full border border-primary-300 bg-transparent py-[3.5px] pl-4 pr-1 text-base font-normal leading-[18px] tracking-m4 hover:border-primary-200 hover:bg-primary-200 hover:text-primary-300  md:ml-0">
       {content}
       <span className="flex-center h-[32px] w-[32px] rounded-full  bg-primary-300 text-primary-200 hover:border-none">
         <Icon
